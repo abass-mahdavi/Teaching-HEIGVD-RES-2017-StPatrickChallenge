3b4077a28ab10706e43bf6348962dc2e4d5318a3
Abass Mahdavi <abass.mahdavi@heig-vd.ch>
2017-03-20 12:40:13 +0100

    Sorry but that's all

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
@@ -1,5 +1,10 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+
 
 /**
  * 
@@ -15,5 +20,15 @@ public class Application {
 
   IStreamDecoratorController getStreamDecoratorController() {
     return null;
+  }
+  
+  public void process (StringReader inputReader,  StringWriter outputWriter){
+      outputWriter.write(inputReader.toString());
+      try{
+            outputWriter.close();
+      }catch (Exception e){
+          
+      }
+      
   }
 }
